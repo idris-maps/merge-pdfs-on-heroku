2568ea2df936e1474c36223c93d556b1e656574c
Anders <idris.maps@gmx.ch>
2018-04-11 13:38:50 +0200

    Add merge endpoint

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const env = require('./lib/env')
+const getPdfs = require('./lib/getPdfBuffers')
+const merge = require('./lib/mergePdfs')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+
+app.post('/', (req, res) => {
+  const pdfUrls = req.body.pdfUrls
+  if (!pdfUrls || !Array.isArray(pdfUrls)) {
+    return res.status(400).json({ message: 'NO "pdfUrls" IN BODY' })
+  }
+  getPdfs(pdfUrls)
+    .then(merge)
+    .then(buffer => {
+      res.status(200).json(buffer)
+    })
+})
+
+app.listen(env.port, () => {
+  console.log(`Listening on ${env.port}`)
+})
