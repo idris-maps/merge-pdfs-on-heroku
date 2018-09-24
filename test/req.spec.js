6aac34cb2dddd65c6edb0f140a8cc59893f7e37f
Anders <idris.maps@gmx.ch>
2018-09-24 14:52:54 +0200

    Remove old stuff

--- test/req.spec.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios')
-const fs = require('fs.promised')
-const data = require('../assets/testUrls')
-const url = 'https://pdf1234.herokuapp.com/'
-
-axios.post(url, { pdfUrls: data }, { responseType: 'arraybuffer' })
-  .then(res => fs.writeFile('assets/merged-from-heroku.pdf', res.data.toString('base64'), { encoding: 'base64' }))
-  .then(console.log)
-  .catch(console.log)
